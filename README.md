75de577fed057069f037946efa25eb1c1f7105a2
Martin Salles <salles.martin@gmail.com>
2017-12-02 00:40:36 +0100

    Access github api

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # github-tools
+
+A tool to gather some data from github through its API.
+
+## Usage
+
+Copy `.env.sample` to `.env` and update values of the environement variables.
--- main.rb
+++ main.rb
@@ -1,5 +1,29 @@
+require 'dotenv/load'
 require 'byebug'
+require_relative './lib/github'
 
-puts 'hi'
+
+HeroNameQuery = Github::Client.parse <<-'GRAPHQL'
+  query {
+    repository(owner:"octocat", name:"Hello-World") {
+      issues(last:20, states:CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first:5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+GRAPHQL
+result = Github::Client.query(HeroNameQuery)
 byebug
 puts 'bye'
