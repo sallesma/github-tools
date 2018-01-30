a5bfb02d5bb9a2f21368bd1612a9bdc9039453f6
Martin Salles <salles.martin@gmail.com>
2018-01-30 17:10:03 +0100

    PR ended_at

--- lib/models/pull_request.rb
+++ lib/models/pull_request.rb
@@ -1,14 +1,16 @@
+require 'active_support/all'
 class PullRequest
-  FIELDS = [:number, :url, :title, :created_at, :merged_at, :closed_at]
+  FIELDS = [:number, :url, :title, :created_at, :merged_at, :closed_at, :ended_at]
   attr_reader(*FIELDS)
 
   def initialize(github_params: {})
     @number = github_params['number']
     @url = github_params['url']
     @title = github_params['title']
-    @created_at = github_params['createdAt']
-    @merged_at = github_params['mergedAt']
-    @closed_at = github_params['closedAt']
+    @created_at = github_params['createdAt']&.to_time
+    @merged_at = github_params['mergedAt']&.to_time
+    @closed_at = github_params['closedAt']&.to_time
+    @ended_at = [@merged_at, @closed_at].min
   end
 
   def self.csv_header
